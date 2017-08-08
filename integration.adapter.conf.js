8f7d87c3d7b1fdc4dbcb9c9ea05d3545ab0b3ded
Manjesh Malavalli <mmalavalli@twilio.com>
2017-08-08 11:37:39 -0700

    Moving twilio-video.js/karma to twilio-webrtc.js/karma

--- integration.adapter.conf.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const makeConf = require('./makeconf');
-const { join } = require('path');
-
-module.exports = makeConf(join('..', 'test', 'integration', 'index.js'), 120000, [
-  require.resolve('webrtc-adapter/out/adapter')
-]);
--- integration.conf.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const makeConf = require('./makeconf');
-const { join } = require('path');
-
-module.exports = makeConf(join('..', 'test', 'integration', 'index.js'), 120000);
