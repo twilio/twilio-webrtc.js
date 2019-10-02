8178dd81c9bf6797eba6dd4b8c08698a62cca719
Manjesh Malavalli <manjesh.github@gmail.com>
2019-10-02 02:21:17 -0700

    Modifying setRemoteDescription test to accept implicit rollback of local offer supported by Firefox 70+.

--- test/integration/spec/rtcpeerconnection.js
+++ test/integration/spec/rtcpeerconnection.js
@@ -1409,6 +1409,11 @@ function testSetDescription(local, signalingState, sdpType) {
         'have-local-offer': 'stable'
       },
       offer: {
+        // NOTE(mmalavalli): Starting from Firefox 70, calling setRemoteDescription()
+        // from .signalingState "have-local-offer" will result in an implicit rollback
+        // of the local offer instead of raising an exception.
+        // Bugzilla: https://bugzilla.mozilla.org/show_bug.cgi?id=1567951
+        ...(firefoxVersion > 69 ? { 'have-local-offer': 'have-remote-offer' } : {}),
         'have-remote-offer': 'have-remote-offer',
         stable: 'have-remote-offer'
       },
@@ -1500,8 +1505,8 @@ function testSetDescription(local, signalingState, sdpType) {
           assertEqualDescriptions(test.peerConnection.localDescription, nextDescription);
         });
       } else {
-        it('should not change .localDescription', () => {
-          assertEqualDescriptions(test.peerConnection.localDescription, localDescription);
+        it(`should ${firefoxVersion > 69 && sdpType === 'offer' ? 'roll back' : 'not change'} .localDescription`, () => {
+          assertEqualDescriptions(test.peerConnection.localDescription, firefoxVersion > 69 && sdpType === 'offer' ? null : localDescription);
         });
       }
 
